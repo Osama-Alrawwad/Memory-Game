b273033427ac3430fb8e48c1ba925f51b6cdff21
Osama ALrawwad <osama.alrawwad1@gmail.com>
2025-11-17 23:56:45 +0300

    V.2 Improvements to all files

--- main.js
+++ main.js
@@ -1,19 +1,38 @@
-let btn = document.querySelector(".splash-screen span");
+let duration = 1000;
+let blockContainer = document.querySelector(".game-blocks");
+let blocks = Array.from(blockContainer.children);
+let ordarRange = Array.from(Array(blocks.length).keys());
+let btn = document.querySelector(".splash-screen span"),
+  splashScreen = document.querySelector(".splash-screen"),
+  nameInput = document.querySelector(".splash-screen input");
+let timeLeft = 60;
+let countdownEl = document.querySelector("#countdown span");
+let resStr = "";
+let restartBtn = document.querySelector(".restart-screen .restart");
+let resText = document.querySelector(".restart-screen h2 span");
+let trise = document.querySelector(".data-info .tries span");
+let timerInterval;
+nameInput.focus();
+
 btn.onclick = function () {
-  let yourName = prompt("Whats Your Name");
+  let yourName = nameInput.value;
   if (yourName == null || yourName == "") {
     yourName = "Unknown";
     document.querySelector(".data-info .name span").innerHTML = yourName;
   } else {
     document.querySelector(".data-info .name span").innerHTML = yourName;
   }
-  document.querySelector(".splash-screen").remove();
+  splashScreen.style.display = "none";
+  blocks.forEach((block, index) => {
+    block.classList.add("is-flipped");
+  });
+  let Show_card = setTimeout(() => {
+    blocks.forEach((block, index) => {
+      block.classList.remove("is-flipped");
+    });
+  }, duration / 2);
+  startTimer();
 };
-
-let duration = 1000;
-let blockContainer = document.querySelector(".game-blocks");
-let blocks = Array.from(blockContainer.children);
-let ordarRange = Array.from(Array(blocks.length).keys());
 
 shuffle(ordarRange);
 blocks.forEach((block, index) => {
@@ -53,7 +72,6 @@ function stopClick() {
   }, duration);
 }
 function cheackMath(firstBlock, secuondBlock) {
-  let trise = document.querySelector(".data-info .tries span");
   if (firstBlock.dataset.pic === secuondBlock.dataset.pic) {
     firstBlock.classList.remove("is-flipped");
     secuondBlock.classList.remove("is-flipped");
@@ -66,4 +84,46 @@ function cheackMath(firstBlock, secuondBlock) {
       secuondBlock.classList.remove("is-flipped");
     }, duration);
   }
+  if (parseInt(trise.innerHTML) >= 10) {
+    resStr = "lose";
+    resultFinish(resStr);
+  }
+  let isFinished = blocks.every((block) =>
+    block.classList.contains("is-match")
+  );
+  if (isFinished) {
+    resStr = "Win";
+    resultFinish(resStr);
+  }
 }
+
+function resultFinish(resStr) {
+  clearInterval(timerInterval);
+  blocks.forEach((block) => block.classList.remove("is-match"));
+  document.querySelector(".restart-screen").style.display = "flex";
+  if (resStr === "Win") {
+    resText.innerHTML = resStr;
+    resText.style.color = "green";
+  } else {
+    resText.innerHTML = resStr;
+    resText.style.color = "red";
+  }
+  document.querySelector(".restart-screen h3 span").innerHTML =
+    trise.textContent;
+  restartBtn.addEventListener("click", () => location.reload());
+}
+function startTimer() {
+  timerInterval = setInterval(() => {
+    countdownEl.innerHTML = timeLeft < 10 ? "0" + timeLeft : timeLeft;
+    timeLeft--;
+    if (timeLeft < 10) {
+      countdownEl.style.color = "red";
+    }
+    if (timeLeft < 0) {
+      clearInterval(timerInterval);
+      resStr = "Lose";
+      resultFinish(resStr);
+    }
+  }, 1000);
+}
+
